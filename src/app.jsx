3a9262fdffbdbc83a21d60a5b9597a86bcc2f0b1
Rémi <remi.van-boxem@student.junia.com>
2024-04-23 11:09:43 +0200

    fix: add ESLint files
    ESLint files has been added to check the code rules. Multiples import has been fixed.
    The main title of the card has been changed from "Secure Roam Companion quick start guide" to "Quick start guide".

--- src/app.jsx
+++ src/app.jsx
@@ -1,34 +1,10 @@
-/*
- * This file is part of Cockpit.
- *
- * Copyright (C) 2017 Red Hat, Inc.
- *
- * Cockpit is free software; you can redistribute it and/or modify it
- * under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation; either version 2.1 of the License, or
- * (at your option) any later version.
- *
- * Cockpit is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with Cockpit; If not, see <http://www.gnu.org/licenses/>.
- */
-
 import cockpit from 'cockpit';
 import React from 'react';
 import { Alert } from "@patternfly/react-core/dist/esm/components/Alert/index.js";
 import { Card, CardBody, CardTitle } from "@patternfly/react-core/dist/esm/components/Card/index.js";
-import { Title } from '@patternfly/react-core/dist/esm/components/Title/index.js';
-import { Skeleton } from '@patternfly/react-core';
-import { Wizard, WizardStep } from '@patternfly/react-core';
-import { Button } from '@patternfly/react-core';
+import { Wizard, WizardStep, Button } from '@patternfly/react-core';
 
 const _ = cockpit.gettext;
-
-
 
 export class Application extends React.Component {
     constructor() {
@@ -40,13 +16,11 @@ export class Application extends React.Component {
         });
     }
 
-
-
     render() {
         return (
             <div>
                 <Card>
-                    <CardTitle component="h1">Secure Roam Companion quick start guide</CardTitle>
+                    <CardTitle component="h1">Quick start guide</CardTitle>
                     <CardBody>
                         Discover the Secure Roam Companion and learn how to use it.
                     </CardBody>
